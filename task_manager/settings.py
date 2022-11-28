879718a8ed90461a591c19304e54c384e20684b2
a.cheremushkin <a.ch3re359@yandex.ru>
2022-11-28 19:04:31 +0400

    All works was done. Try deploy on railway

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -21,7 +21,7 @@ DATABASE_CONFIG = dj_database_url.config(default=os.getenv('DATABASE_URL'))
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('DJANGO_SECRETKEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG')
 
